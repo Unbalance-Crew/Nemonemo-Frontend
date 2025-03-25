e14a8c90dd8148475a0deaa1220dfaaf06496c36
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-25 14:08:20 +0900

    feat: header footer

---
+++ src/components/common/footer/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style';
+
+const Footer = () => {
+    return (
+        <S.Container>
+            <div>푸터 영역</div>
+        </S.Container>
+    );
+};
+
+export default Footer;
---
+++ src/components/common/header/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style';
+
+const Header = () => {
+    return (
+        <S.Container>
+            <div>헤더 영역</div>
+        </S.Container>
+    );
+};
+
+export default Header;

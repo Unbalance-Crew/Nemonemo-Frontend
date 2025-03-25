20033a1a62b1519c6f0c43837c0165972dae5f21
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-25 23:19:42 +0900

    feat: delete

--- src/libs/axios/customAxios.ts
+++ src/libs/axios/customAxios.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import { AxiosError } from 'axios';
 import { ACCESS_TOKEN, REFRESH_TOKEN, REQUEST_TOKEN } from '@/constants/token/token.constants';
 import { Token } from '@/libs/token/session';
 
@@ -25,6 +26,7 @@ const nemonemoAxios = createCustomAxiosInstance({
 });
 
 let isRefreshing = false;
+
 let refreshSubscribers: (( token: string ) => void)[] = [];
 
 const onRefreshed = ( token: string ) => {
@@ -37,52 +39,11 @@ const addRefreshSubscriber = ( callback: ( token: string ) => void ) => {
 };
 
 nemonemoAxios.interceptors.request.use(
-    (config) => {
-        const token = Token.getToken(ACCESS_TOKEN);
-        if (token) {
-            config.headers[REQUEST_TOKEN] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    },
+
 );
 
 nemonemoAxios.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (error) => {
-        const originalRequest = error.config;
-        if (error.response?.status === 401) {
-            if (!isRefreshing) {
-                isRefreshing = true;
-                try {
-                    const refreshToken = Token.getToken(REFRESH_TOKEN);
-                    const response = await axios.post(`${SERVER_URL}/api/auth/reissue`, {
-                        refreshToken,
-                    });
-                    const newAccessToken = response.data.accessToken;
-                    Token.setToken(ACCESS_TOKEN, newAccessToken);
-                    onRefreshed(newAccessToken);
-                    originalRequest.headers[REQUEST_TOKEN] = `Bearer ${newAccessToken}`;
-                    return nemonemoAxios(originalRequest);
-                } catch (error) {
-                    return Promise.reject(error);
-                } finally {
-                    isRefreshing = false;
-                }
-            }
-            return new Promise((resolve) => {
-                addRefreshSubscriber((token) => {
-                    originalRequest.headers[REQUEST_TOKEN] = `Bearer ${token}`;
-                    resolve(nemonemoAxios(originalRequest));
-                });
-            });
-        }
-        return Promise.reject(error);
-    },
+
 );
 
 export default nemonemoAxios;

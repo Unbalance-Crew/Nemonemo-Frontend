3f85076e376abe768c46e4793081e12fae40bb3b
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-25 19:25:24 +0900

    feat: auth type

--- src/types/auth/auth.type.ts
+++ src/types/auth/auth.type.ts
@@ -1,4 +1,19 @@
-export interface LoginType {
+export interface Login {
     username: string;
     password: string;
+};
+
+export interface SignUp {
+    username: string;
+    password: string;
+    athority?: string;
+};
+
+export interface AuthResponse {
+    accessToken: string;
+    refreshToken: string;
+};
+
+export interface NewAccessToken {
+    accessToken: string;
 };

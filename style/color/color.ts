ce54361b1b7b1ae3933475d742a55b8c7e9db3b4
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-26 09:15:44 +0900

    feat: color

---
+++ style/color/color.ts
@@ -0,0 +1,14 @@
+export const COLOR = {
+    Main: "#796BE6",
+    Serve: "#281F69",
+    Red: "#FF6C6C",
+    Dark: "#2C2F3A",
+    Gray: "#B7B7B7",
+    LightGray:"#F4F4F4",
+    Green: "#30FF30",
+    Black: "#000000",
+    White: "#FFFFFF",
+    Background: "#FFFFFF",
+    LoginBackground: "#F6F7F9",
+    Placeholder: "rgba(0, 0, 0, 0.5)",
+};

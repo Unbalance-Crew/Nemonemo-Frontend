a7bf96878a33f8ed0b8008e6b5cb7478ff50f9a6
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-26 12:37:57 +0900

    feat: customAxios

--- src/libs/axios/customAxios.ts
+++ src/libs/axios/customAxios.ts
@@ -1,2 +1,38 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import { ACCESS_TOKEN, REFRESH_TOKEN, REQUEST_TOKEN } from '@/constants/token/token.constants';
+import { BaseResponse } from '@/types/response/base.response';
+import { TokenResponse } from '@/types/auth/auth';
+
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+    retry?: number;
+};
+
+const nemonemoAxios = axios.create({
+    baseURL: process.env.VITE_SERVER_URL,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+    },
+    withCredentials: true,
+});
+
+let isRefreshing = false;
+let refreshSubscribers: ((token: string) => void)[] = [];
+
+const onRefreshed = (token: string) => {
+    refreshSubscribers.forEach((callback) => callback(token));
+    refreshSubscribers = [];
+};
+
+const addRefreshSubscriber = (callback: (token: string) => void) => {
+    refreshSubscribers.push(callback);
+};
+
+nemonemoAxios.interceptors.request.use(
+
+);
+
+nemonemoAxios.interceptors.response.use(
+
+);
+
+export default nemonemoAxios;

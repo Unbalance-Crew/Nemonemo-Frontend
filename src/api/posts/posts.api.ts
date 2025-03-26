0563396c1b266197573e7237a445a0a58a179229
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-27 08:19:05 +0900

    feat: post api

--- src/api/posts/posts.api.ts
+++ src/api/posts/posts.api.ts
@@ -2,3 +2,11 @@ import { nemonemoAxios } from "@/libs/axios/axios";
 
 const SERVER_URL = process.env.VITE_SERVER_URL;
 
+export const getPosts = async (postId: number) =>{
+    try {
+        const { data } = await nemonemoAxios.get(`${SERVER_URL}/api/posts/${postId}`);
+        return data;
+    } catch (error) {
+        throw new Error("게시글 로딩 실패");
+    };
+};

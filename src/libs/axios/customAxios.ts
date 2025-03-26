b21d1e6628690d9bad5d4ede3b5de044286ef46c
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-26 15:30:35 +0900

    feat: customAxios request interceptor

--- src/libs/axios/customAxios.ts
+++ src/libs/axios/customAxios.ts
@@ -2,9 +2,10 @@ import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import { ACCESS_TOKEN, REFRESH_TOKEN, REQUEST_TOKEN } from '@/constants/token/token.constants';
 import { BaseResponse } from '@/types/response/base.response';
 import { TokenResponse } from '@/types/auth/auth';
+import { Token } from '@/libs/token/session';
 
 interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
-    retry?: number;
+    retry?: boolean;
 };
 
 const nemonemoAxios = axios.create({
@@ -28,7 +29,23 @@ const addRefreshSubscriber = (callback: (token: string) => void) => {
 };
 
 nemonemoAxios.interceptors.request.use(
+    async (config) => {
+        const token = Token.getToken(ACCESS_TOKEN);
+        if (token) {
+            config.headers[REQUEST_TOKEN] = `Bearer ${token}`;
+        };
 
+        if (config.data instanceof FormData) {
+            config.headers["Content-Type"] = "multipart/form-data";
+        } else {
+            config.headers["Content-Type"] = "application/json";
+        };
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    },
 );
 
 nemonemoAxios.interceptors.response.use(

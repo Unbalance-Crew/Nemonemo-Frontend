5bcfbc1c29b377cc12de4b17c99907af14422659
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-26 20:20:13 +0900

    feat: 임포트 정렬

--- src/libs/axios/request.interceptor.ts
+++ src/libs/axios/request.interceptor.ts
@@ -1,6 +1,10 @@
 import { InternalAxiosRequestConfig } from "axios";
 import { Token } from "@/libs/token/token";
-import { ACCESS_TOKEN, REFRESH_TOKEN, REQUEST_TOKEN } from "@/constants/token/token.constants";
+import { 
+    ACCESS_TOKEN, 
+    REFRESH_TOKEN, 
+    REQUEST_TOKEN 
+} from "@/constants/token/token.constants";
 
 export const requestInterceptor = (
     config: InternalAxiosRequestConfig
--- src/libs/axios/response.interceptor.ts
+++ src/libs/axios/response.interceptor.ts
@@ -1,6 +1,10 @@
 import { AxiosError } from "axios";
 import { Token } from "@/libs/token/token";
-import { ACCESS_TOKEN, REFRESH_TOKEN, REQUEST_TOKEN } from "@/constants/token/token.constants";
+import { 
+    ACCESS_TOKEN, 
+    REFRESH_TOKEN, 
+    REQUEST_TOKEN
+} from "@/constants/token/token.constants";
 import { refresh } from "@/api/auth/auth.api";
 import { nemonemoAxios } from "@/libs/axios/axios";
 

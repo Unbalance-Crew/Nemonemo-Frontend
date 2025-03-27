5fa01f2811c401bb4c294f1d14a35bd91a1c352b
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-27 23:31:48 +0900

    feat: 라우팅 설정

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,25 +1,11 @@
 import * as S from './style';
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import Header from '@/components/common/header';
 
 const Layout = () => {
-    const [ showHeader, setShowHeader ] = useState(true);
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const path = window.location.pathname;
-        if (path === '/login' || path === '/register') {
-            setShowHeader(false);
-        } else {
-            setShowHeader(true);
-        };
-    }, [navigate]);
-
     return (
         <S.Container>
-            {showHeader && <Header />}
+            <Header />
             <S.Content>
                 <Outlet />
             </S.Content>

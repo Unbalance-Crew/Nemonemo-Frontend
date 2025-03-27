2b715790741c2eda0a5952111f8afe4f5f6724ff
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-27 15:01:52 +0900

    feat: 라우팅 설정

--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -3,6 +3,8 @@ import Layouts from '@/layouts';
 import Home from '@/pages/home';
 import Login from '@/pages/login';
 import Register from '@/pages/register';
+import Search from '@/pages/search';
+import Create from '@/pages/create';
 
 const Router = () => {
     return (
@@ -10,6 +12,8 @@ const Router = () => {
             <Routes>
                 <Route path='/' element={<Layouts />}>
                     <Route index element={<Home />} />
+                    <Route path='/search' element={<Search />} />
+                    <Route path='/create' element={<Create />} />
                 </Route>
                 <Route path='/login' element={<Login />} />
                 <Route path='/register' element={<Register />} />
---
+++ src/pages/create/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style';
+
+const Create = () => {
+    return (
+        <S.Container>
+            <div>게시물 생성</div>
+        </S.Container>
+    );
+};
+
+export default Create;

99f71e21bcd16037f979d31591d88ed11e624834
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-27 14:52:57 +0900

    feat: setting

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style';
+
+const Profile = () => {
+    return (
+        <S.Container>
+            <div>프로필</div>
+        </S.Container>
+    );
+};
+
+export default Profile;
---
+++ src/pages/search/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style';
+
+const Search = () => {
+    return (
+        <S.Container>
+            <div>검색</div>
+        </S.Container>
+    );
+};
+
+export default Search;

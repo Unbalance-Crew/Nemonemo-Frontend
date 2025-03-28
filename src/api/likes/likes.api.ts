df52232b24c263e2050d05f8f0865433443e948d
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-28 09:59:05 +0900

    feat: header style

--- src/api/likes/likes.api.ts
+++
@@ -1,3 +0,0 @@
-import { nemonemoAxios } from "@/libs/axios/axios";
-
-const SERVER_URL = process.env.VITE_SERVER_URL;
--- src/components/common/header/style.ts
+++ src/components/common/header/style.ts
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 export const Container = styled.header`
     width: 100%;
     height: 3.5rem;
-    background-color: #F0F0F0;
+    background-color: #FFFFFF;
     position: absolute;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
     top: 0;
     left: 0;
     display: flex;
@@ -42,5 +42,11 @@ export const MenuItem = styled.li`
     font-size: 15px;
     cursor: pointer;
     padding: 10px 15px;
-    font-weight: 400;
+    font-weight: 300;
+    border-radius: 10px;
+
+    &:hover {
+        background-color: #E0E0E0;
+        transition: 0.4s;
+    }
 `

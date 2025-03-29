e576a2e6b3079a526d26d54961689e292b05a319
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-29 09:14:47 +0900

    feat: useLogout

--- src/hooks/auth/useLogout.ts
+++ src/hooks/auth/useLogout.ts
@@ -3,7 +3,16 @@ import { Token } from '@/libs/token/token';
 import Toast from '@/libs/toast/toast';
 
 const useLogout = () => {
+    const navigate = useNavigate();
 
+    const handleLogout = () => {
+        Token.clearToken();
+        navigate('/login');
+        Toast('로그아웃 되었습니다!', 'SUCCESS');
+    };
+    return {
+        handleLogout,
+    }
 };
 
 export default useLogout;

42e54d297f374edb07f4d436d24714feacceabc4
윤효진 <hyojinee@dgsw.hs.kr>
2025-03-29 23:51:58 +0900

    feat: 포스트 박스 프로필 박스 섹션

---
+++ src/components/post/postbox/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style';
+
+const PostBox = () => {
+    return (
+        <S.Container>
+            <div></div>
+        </S.Container>
+    );
+};
+
+export default PostBox;
---
+++ src/components/profile/profilebox/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style';
+
+const ProfileBox = () => {
+    return (
+        <S.Container>
+            <div></div>
+        </S.Container>
+    );
+};
+
+export default ProfileBox;

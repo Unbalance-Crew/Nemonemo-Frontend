69c3c7037dc416fbb99c1280d153a99f1c25a8d6
윤효진 <hyojinee@dgsw.hs.kr>
2025-04-01 13:13:49 +0900

    feat: search api

---
+++ src/api/search/search.api.ts
@@ -0,0 +1,15 @@
+import { motreeAxios } from "@/libs/axios/axios";
+import { SearchResponse } from "@/types/search/search";
+
+const SERVER_URL = process.env.VITE_SERVER_URL || "http://localhost:5173";
+
+export const search = async (requestDto: { keyword: string; page: number; size: number }): Promise<SearchResponse> => {
+    try {
+        const { data } = await motreeAxios.get<SearchResponse>(`${SERVER_URL}/api/posts/search`, {
+            params: requestDto,
+        });
+        return data;
+    } catch (error) {
+        throw new Error("검색 요청 실패");
+    };
+};

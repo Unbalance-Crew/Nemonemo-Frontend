d41e2c4b145cdb9099d49ce7af09f2b93b9329e7
윤효진 <hyojinee@dgsw.hs.kr>
2025-04-01 13:32:45 +0900

    feat: 댓글 작성 조회 api 호출

--- src/api/commnets/comments.api.ts
+++ src/api/commnets/comments.api.ts
@@ -1,4 +1,24 @@
 import { motreeAxios } from "@/libs/axios/axios";
-import { CommentResponse } from "@/types/comments/comments";
+import { createCommentResponse, CommentResponse, createCommentRequest } from "@/types/comments/comments";
 
 const SERVER_URL = process.env.VITE_SERVER_URL || "http://localhost:5173";
+
+export const createComment = async (postId: number, requestDto: createCommentRequest): Promise<createCommentResponse> => {
+    try {
+        const { data } = await motreeAxios.post<createCommentResponse>(`${SERVER_URL}/api/posts/${postId}/comments`, requestDto);
+        return data;
+    } catch (error) {
+        throw new Error("댓글 작성 요청 실패");
+    };
+};
+
+export const getComments = async (postId: number, pageRequestDto: { page: number; size: number }): Promise<CommentResponse> => {
+    try {
+        const { data } = await motreeAxios.get<CommentResponse>(`${SERVER_URL}/api/posts/${postId}/comments`, {
+            params: pageRequestDto,
+        });
+        return data;
+    } catch (error) {
+        throw new Error("댓글 조회 요청 실패");
+    };
+};

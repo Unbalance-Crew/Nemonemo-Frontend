d99d49191f4924e08442ccdb7d7fb257b889daeb
윤효진 <hyojinee@dgsw.hs.kr>
2025-04-03 15:21:09 +0900

    feat: private route

---
+++ src/components/profile/modal/style.ts
@@ -0,0 +1,5 @@
+import styeld from 'styled-components';
+
+export const Container = styeld.div`
+
+`
--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PrivateRoute from './privateRoute';
 import Layout from '@/layouts';
 import Home from '@/pages/home';
 import Login from '@/pages/login';
@@ -6,20 +7,25 @@ import Register from '@/pages/register';
 import Search from '@/pages/search';
 import Create from '@/pages/create';
 import Profile from '@/pages/profile';
+import NotFound from '@/pages/notfound';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path='/' element={<Home />} />
-                    <Route path='/search' element={<Search />} />
-                    <Route path='/create' element={<Create />} />
-                    <Route path='/profile' element={<Profile />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+
+                <Route element={<PrivateRoute />}>
+                    <Route element={<Layout />}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/search" element={<Search />} />
+                        <Route path="/create" element={<Create />} />
+                        <Route path="/profile/:username" element={<Profile />} />
+                    </Route>
                 </Route>
-                <Route path='/login' element={<Login />} />
-                <Route path='/register' element={<Register />} />
+
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     );

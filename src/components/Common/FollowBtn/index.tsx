b86a90d380faeab4156ee8fcb70d2a6af181fa50
윤효진 <hyojinee@dgsw.hs.kr>
2025-04-14 20:13:20 +0900

    updata: profile page

---
+++ src/components/Common/FollowBtn/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style';
+
+const FollowBtn = () => {
+    return (
+        <S.Container>
+            <div></div>
+        </S.Container>
+    );
+};
+
+export default FollowBtn;
---
+++ src/components/Profile/InfoBox/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style';
+
+const InfoBox = () => {
+    return (
+        <S.Container>
+            <div></div>
+        </S.Container>
+    );
+};
+
+export default InfoBox;
